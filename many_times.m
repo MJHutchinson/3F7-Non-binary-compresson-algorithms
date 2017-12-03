8a601897e7db5925bf77a315c022d41c1cd76a78
Basil Mustafa <BasilAMustafa@gmail.com>
2017-12-03 15:03:13 +0000

    add figures for time

--- many_times.m
+++ many_times.m
@@ -3,9 +3,10 @@ files = dir(fol);
 files(1) = [];
 
 %compute bases
-b = 2.^(1:24);
-b = [b ceil(linspace(2^1, 2^24, 1000)) 2:1:50];
-b = sort(unique(b));
+b = [2.^(1:16) 2:1:50];
+%b = [b linspace(2^1, 2^16, 942) 2:1:50];
+b = sort(unique(round(b)));
+disp(['Testing coding on ' num2str(length(files)) ' files with ' num2str(length(b)) ' bases'])
 
 l = length(b) + 1;
 t = cell(l, (2*length(files) + 1));
@@ -14,22 +15,18 @@ for k = 2:l
 end
 
 for o = 2:length(files)
+    disp(['testing for ' files(o).name])
+    
     t{1, 2*o} = files(o).name;
     f_name = [fol files(o).name];
     t_temp = compare_times(f_name, b);
     
     %copy values
     for k = 2:l
-        if t_temp(k - 1, 2)*t_temp(k-1,3) < 0
-            %remove this base from the list
-            disp(['Base ' num2str(b(k - 1)) ' failed on file ' files(o).name ' - removing from future tests'])
-            b(k - 1) = [];
-            t(k, :) = [];
-        else
-            t{k, 2*o} = t_temp(k - 1, 2);
-            t{k, 2*o+1} = t_temp(k - 1, 3);
-        end
+        t{k, 2*o} = t_temp(k - 1, 2);
+        t{k, 2*o+1} = t_temp(k - 1, 3);
     end
     
     %t{2:l, 2*o:1+2*o} = t_temp;
 end
+save('data\times.mat','t')
